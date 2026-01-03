0121232ffde7e7b4c31296129890a777c7cfef6a
Máté Zsebeházi <zsebehazimate@gmail.com>
2026-01-03 14:02:49 +0100

    Add AppDbContext with Colors DbSet

---
+++ szines.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using szines.API.Models;
+
+namespace szines.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<ColorEntity> Colors => Set<ColorEntity>();
+    }
+}
