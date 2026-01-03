c5a9cb5c391f86413904164d76b674c61405bb06
Máté Zsebeházi <zsebehazimate@gmail.com>
2026-01-03 14:03:03 +0100

    Add ColorEntity model with basic properties

---
+++ szines.API/Models/ColorEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace szines.API.Models
+{
+    public class ColorEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string HexValue { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
