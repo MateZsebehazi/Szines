a004413d048184356ef611e713f910f1be950481
Máté Zsebeházi <zsebehazimate@gmail.com>
2026-01-03 14:09:45 +0100

    Add ColorsController with GET and POST endpoints

---
+++ szines.API/Controllers/ColorsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using szines.API.Data;
+using szines.API.Models;
+
+namespace szines.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ColorsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ColorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var colors = await _context.Colors
+                .OrderByDescending(c => c.CreatedAt)
+                .ToListAsync();
+
+            return Ok(colors);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ColorEntity color)
+        {
+            _context.Colors.Add(color);
+            await _context.SaveChangesAsync();
+            return Ok(color);
+        }
+    }
+}
